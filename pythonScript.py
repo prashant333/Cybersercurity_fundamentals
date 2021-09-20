4189b8f65bba8522d01d5c452ac6ac38fafdca22
prashant333 <pmani157@gmail.com>
2021-09-20 14:11:39 +0530

    first python script

---
+++ pythonScript.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+""" Please make sure that the python is installed at the above given PATH, else change the above line to specific
+location where python is installed."""
+ip = raw_input("Enter the IP: ")
+port = input("Enter the port: ")
