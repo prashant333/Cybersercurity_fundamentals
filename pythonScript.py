9486aca76b88f2b5e0cc2d7c47a1a7849938df1c
prashant333 <pmani157@gmail.com>
2021-09-21 10:45:42 +0530

    checking if a given port is open or closed.

--- pythonScript.py
+++ pythonScript.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 """ Please make sure that the python is installed at the above given PATH, else change the above line to specific
 location where python is installed."""
+
+import socket
 ip = raw_input("Enter the IP: ")
 port = input("Enter the port: ")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if s.connect_ex((ip, port)):
+    print "Port", port, "is closed"
+else:
+    print "Port", port, "is open"
