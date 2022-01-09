72605f95c8557d31d8a3c8070e69fac9f3f3861c
prashant333 <pmani157@gmail.com>
2022-01-09 17:45:52 +0530

    code updated for client

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ HOST = '192.168.1.4'
 PORT = 9090
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((HOST, PORT))
+client.connect((HOST, PORT))
 
-socket.send(f"Hello World!".encode('utf-8'))
-print(socket.recv(1024).decode('utf-8'))
+client.send(f"Hello World!".encode('utf-8'))
+print(client.recv(1024).decode('utf-8'))
