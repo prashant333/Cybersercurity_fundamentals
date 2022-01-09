910d7a97f7480faccc3f024400ed96ef8b4f891a
prashant333 <pmani157@gmail.com>
2022-01-09 16:52:10 +0530

    comments updated

--- socket_programming.py
+++ socket_programming.py
@@ -5,6 +5,8 @@ import socket # for socket
 import sys
 
 try:
+	# socket.AF_INET --> specifies that this is an internet socket,
+	# socket.SOCK_STREAM --> specifies that this is an TCP connection
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Socket successfully created")
 except socket.error as err:
@@ -14,6 +16,8 @@ except socket.error as err:
 port = 80
 
 try:
+	# host_ip = socket.gethostbyname(socket.gethostname()) --> this command will automatically retrieve the
+	# ip address of the running computer.
 	host_ip = socket.gethostbyname('www.google.com')
 except socket.gaierror:
 
